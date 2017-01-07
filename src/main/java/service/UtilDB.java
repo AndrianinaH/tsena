35c39ed06f90ceb7087f06c4d4a6ebdbea79907b
Andrianina <rakotoandrianina@gmail.com>
2017-01-07 10:34:51 +0300

    connecxion bdd jsp

---
+++ src/main/java/service/UtilDB.java
@@ -0,0 +1,16 @@
+package service;
+
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class UtilDB {
+	
+	@SuppressWarnings("unused")
+	private static Connection getConnection() throws URISyntaxException, SQLException {
+	    String dbUrl = System.getenv("JDBC_DATABASE_URL");
+	    return DriverManager.getConnection(dbUrl);
+	}
+
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -2,5 +2,6 @@
 <html>
     <body>
         <h2><%= Test34.hello34() %></h2>
+        <h2><%= UtilDB.getConnection() %></h2>
     </body>
 </html>
